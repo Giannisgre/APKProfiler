886c94f177a6826557ff44177ef13a905b21c2c5
Giannisgre <Giannisgre@users.noreply.github.com>
2020-07-26 16:03:19 +0300

    Add files via upload

--- APKProfiler/Decompiler.cs
+++ APKProfiler/Decompiler.cs
@@ -56,6 +56,7 @@ namespace APKProfiler
         {
             manifest = new Manifest();
             manifest.ParseManifest(pathToManifest);
+
         }
 
         public void AnalyzeSmali(string pathToSmali)
